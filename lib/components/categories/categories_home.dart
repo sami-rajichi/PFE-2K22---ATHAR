7ad0b7fbf04af373541cc45bca1f28ae5c91c2ed
sami-rajichi <samirajichi2016@gmail.com>
2022-04-02 13:32:40 +0100

    First attempt on Categories Interface

--- lib/components/categories/categories_home.dart
+++ lib/components/categories/categories_home.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:monumento/d_b_icons_icons.dart';
+import 'package:monumento/shared/components/bottomBar.dart';
+import 'package:monumento/shared/components/menu_widget.dart';
+import 'package:monumento/shared/components/neumorphism.dart';
 import 'package:syncfusion_flutter_maps/maps.dart';
 
 class HomeCategories extends StatefulWidget {
@@ -9,28 +14,59 @@ class HomeCategories extends StatefulWidget {
 }
 
 class _HomeCategoriesState extends State<HomeCategories> {
-  late MapShapeSource _dataSource;
 
-  @override
-  void initState() {
-    _dataSource = MapShapeSource.asset(
-      'assets/Map/Tunisia.json',
-      shapeDataField: 'name',
-    );
-    super.initState();
-  }
+  Map<String, String> classes = {
+    'nord' : 'assets/map/nord.png',
+    'centre' : 'assets/map/centre.png',
+    'sud' : 'assets/map/sud.png',
+  };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(15),
-        child: SfMaps(
-          layers: [
-            MapShapeLayer(source: _dataSource),
-          ],
+      appBar: AppBar(
+        leading: MenuWidget(),
+        title: Text(
+          'Monumento'
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: neumorphicButton(
+          child: Icon(DBIcons.search,),
+          prColor: Colors.indigo.shade500,
+          sdColor: Colors.indigo.shade800),
+          )
+        ],
+        backgroundColor: Colors.indigo[500],
       ),
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/map/nord.png',
+            height: 700,
+            fit: BoxFit.cover,
+          ),
+          Image.asset(
+            'assets/map/centre.png',
+            height: 700,
+            fit: BoxFit.cover,
+          ),
+          Image.asset(
+            'assets/map/sud.png',
+            height: 700,
+            fit: BoxFit.cover,
+          ),
+          
+        ],
+      ),
+      bottomNavigationBar: ConvexBottomBar(
+        backgroundColor: Colors.indigo[500]
+      )
     );
+  }
+
+  clickable_class(String c, String clickable){
+    
   }
 }
